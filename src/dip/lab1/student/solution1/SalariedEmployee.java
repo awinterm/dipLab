dba562db051ed1a2663e7367bf08c83bafe494ad
andre_000 <andre_000@DrewsLaptop.PK5001Z>
2015-09-20 22:58:15 -0500

    I think this class is good to go.

--- src/dip/lab1/student/solution1/SalariedEmployee.java
+++ src/dip/lab1/student/solution1/SalariedEmployee.java
@@ -8,21 +8,29 @@ package dip.lab1.student.solution1;
  *
  * @author your name goes here
  */
-public class SalariedEmployee extends Employee {
+public class SalariedEmployee implements Employee {
 
     private double annualSalary;
     private double annualBonus;
     
     
     
-    
-    /** default constructor. Is this the best way to go? */
-    public SalariedEmployee() {}
+    /** default constructor. Is this the best way to go? 
+     This is the fragility thing we were talking about. Using this would let you 
+     * create an instance of this class with null values. You would have to know to set them
+     * before doing anything else. Forcing them to be declared in the constructor means the other methods should work.
+     * 
+     
+     */
+  
 
     /**
      * Convenience constructor. Is this the best way to go?
      * @param annualSalary - the employee's annual salary
      * @param annualBonus - a bonus benefit, if any
+     * 
+     * This one forces the user to set values to these properties. So you can't get 
+     * null values later.
      */
     public SalariedEmployee(double annualSalary, double annualBonus) {
         setAnnualSalary(annualSalary);
@@ -31,25 +39,38 @@ public class SalariedEmployee extends Employee {
 
     
         @Override
-    public double getAnnualWages(){
+    public final double getAnnualWages(){
         return (annualSalary + annualBonus);
     };
     
     
-        public double getAnnualBonus() {
+        public final double getAnnualBonus() {
         return annualBonus;
     }
         
-         public void setAnnualBonus(double annualBonus) {
-        this.annualBonus = annualBonus;
-    }
-        public double getAnnualSalary() {
+         public final void setAnnualBonus(double annualBonus) {
+        
+             if(annualBonus < 0 || annualBonus > 1000000000){
+                 System.out.println("I don't know about throwing exceptions yet "
+                         + "or really what throwing really does. So better not use it "
+                         + "untill Jim explains it better. If your bonus is more then 10 million dollars "
+                         + "You probably don't need it.");
+             }
+             else{
+             this.annualBonus = annualBonus;
+             }
+         }
+        public final double getAnnualSalary() {
         return annualSalary;
     }
-       public void setAnnualSalary(double annualSalary) {
+       public final void setAnnualSalary(double annualSalary) {
+             if(annualSalary < 0 ){
+                 System.out.println("Paying to work is a bad practice");
+             }
+             else{
         this.annualSalary = annualSalary;
-    }
-    
+        }
+        }
     
     
     
