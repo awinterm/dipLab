9e8be2223cd94cbc5fed2a7ef09172aa267e1bfd
andre_000 <andre_000@DrewsLaptop.PK5001Z>
2015-09-20 22:34:06 -0500

    First Commit

---
+++ src/dip/lab1/student/solution1/Employee.java
@@ -0,0 +1,80 @@
+package dip.lab1.student.solution1;
+
+
+
+/**
+ * The concrete super class of all employee types. What's wrong with this class?
+ * Find and fix all encapsulation, DIP and best practice problems learned
+ * so far.
+ *
+ * @author your name goes here
+ */
+public abstract class Employee {
+    
+    public abstract double getAnnualWages();
+
+    /**
+     * Returns annual bonus for salaried workers. Is this polymorphic?
+     * Could it be? Does it belong here?
+     *
+     * @return annual bonus or zero if none.
+     */
+
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @param annualBonus -- think carefully about this
+     */
+    public void setAnnualBonus(double annualBonus) {
+        this.annualBonus = annualBonus;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @return
+     */
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @param annualSalary - think carefully about this
+     */
+    public void setAnnualSalary(double annualSalary) {
+        this.annualSalary = annualSalary;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @return
+     */
+    public double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @param hourlyRate -- think carefully about this
+     */
+    public void setHourlyRate(double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @return
+     */
+    public double getTotalHrsForYear() {
+        return totalHrsForYear;
+    }
+
+    /**
+     * Is this polymorphic? Should it be? Does it belong here?
+     * @param totalHrsForYear -- think carefully about this
+     */
+    public void setTotalHrsForYear(double totalHrsForYear) {
+        this.totalHrsForYear = totalHrsForYear;
+    }
+
+}
