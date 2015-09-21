b8ddc6b99fc5856c71773546638b0902b0b00dc5
andre_000 <andre_000@DrewsLaptop.PK5001Z>
2015-09-20 23:00:01 -0500

    I think this interface is good to go. Still debating bonuses for hourly emps.

--- src/dip/lab1/student/solution1/Employee.java
+++ src/dip/lab1/student/solution1/Employee.java
@@ -9,72 +9,8 @@ package dip.lab1.student.solution1;
  *
  * @author your name goes here
  */
-public abstract class Employee {
+public abstract interface Employee {
     
     public abstract double getAnnualWages();
-
-    /**
-     * Returns annual bonus for salaried workers. Is this polymorphic?
-     * Could it be? Does it belong here?
-     *
-     * @return annual bonus or zero if none.
-     */
-
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @param annualBonus -- think carefully about this
-     */
-    public void setAnnualBonus(double annualBonus) {
-        this.annualBonus = annualBonus;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @return
-     */
-    public double getAnnualSalary() {
-        return annualSalary;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @param annualSalary - think carefully about this
-     */
-    public void setAnnualSalary(double annualSalary) {
-        this.annualSalary = annualSalary;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @return
-     */
-    public double getHourlyRate() {
-        return hourlyRate;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @param hourlyRate -- think carefully about this
-     */
-    public void setHourlyRate(double hourlyRate) {
-        this.hourlyRate = hourlyRate;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @return
-     */
-    public double getTotalHrsForYear() {
-        return totalHrsForYear;
-    }
-
-    /**
-     * Is this polymorphic? Should it be? Does it belong here?
-     * @param totalHrsForYear -- think carefully about this
-     */
-    public void setTotalHrsForYear(double totalHrsForYear) {
-        this.totalHrsForYear = totalHrsForYear;
-    }
-
+    
 }
